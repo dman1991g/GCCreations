b05d2de247b414f6e5756c4720c4bb3106c007f3
dman1991g <141966927+dman1991g@users.noreply.github.com>
2025-01-31 20:57:58 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'gc-creations-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/Products.html',
+    '/stylesheet.css',
+    '/script.js' // Add the JavaScript file to the cache
+];
+
+// Install Service Worker and cache files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Activate Service Worker and remove old caches if any
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch resources from the cache first, then try the network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
